a79fc83c2e393889bc50e3f10f9a10a9bc59188e
Alejandro Monroy <amonroym99@gmail.com>
2024-01-15 13:40:37 +0100

    Add non-diverse glass dataset

--- utils.py
+++ utils.py
@@ -4,7 +4,7 @@ global reference_image_category
 CelebA_attribute_list = ['5_o_Clock_Shadow', 'Arched_Eyebrows', 'Attractive', 'Bags_Under_Eyes', 'Bald',
                          'Bangs', 'Big_Lips', 'Big_Nose', 'Black_Hair', 'Blond_Hair',
                          'Blurry', 'Brown_Hair', 'Bushy_Eyebrows', 'Chubby', 'Double_Chin',
-                         'Eyeglasses', 'Goatee', 'Gray_Hair', 'Heavy_Makeup', 'High_Cheekbones',
+                         'Eyeglasses', 'Eyeglasses_non_diverse', 'Goatee', 'Gray_Hair', 'Heavy_Makeup', 'High_Cheekbones',
                           'Male', 'Mouth_Slightly_Open', 'Mustache', 'Narrow_Eyes', 'No_Beard',
                          'Oval_Face', 'Pale_Skin', 'Pointy_Nose', 'Receding_Hairline', 'Rosy_Cheeks',
                           'Sideburns', 'Smiling', 'Straight_Hair', 'Wavy_Hair', 'Wearing_Earrings',
