394d19011c1a428a1c95f5df834e5a3211099d8d
daniel-gallo <danielsantoangel@gmail.com>
2024-02-05 21:00:21 +0100

    Make generate_datasets.py idempotent

--- generate_jobfiles/diversity_eyeglasses/generate_datasets.py
+++ generate_jobfiles/diversity_eyeglasses/generate_datasets.py
@@ -1,33 +1,36 @@
-import os
 from pathlib import Path
 
-ORIGINAL_DATASET_PATH = Path('data/celeba/Eyeglasses')
-NEG_POS_FILENAMES_PATH = Path('FINAL_REPO/generate_jobfiles/diversity_eyeglasses/Eyeglasses_negative_Male_positve.txt')
-POS_POS_FILENAMES_PATH = Path('FINAL_REPO/generate_jobfiles/diversity_eyeglasses/Eyeglasses_positive_Male_positve.txt')
+ORIGINAL_DATASET_PATH = Path("data/celeba/Eyeglasses")
+NEG_POS_FILENAMES_PATH = Path("generate_jobfiles/diversity_eyeglasses/Eyeglasses_negative_Male_positive.txt")
+POS_POS_FILENAMES_PATH = Path("generate_jobfiles/diversity_eyeglasses/Eyeglasses_positive_Male_positive.txt")
 
+neg_filenames = [file.name for file in (ORIGINAL_DATASET_PATH / "negative").iterdir() if file.is_file()]
+pos_filenames = [file.name for file in (ORIGINAL_DATASET_PATH / "positive").iterdir() if file.is_file()]
 
-neg_filenames = os.listdir(os.path.join(ORIGINAL_DATASET_PATH, 'negative'))
-pos_filenames = os.listdir(os.path.join(ORIGINAL_DATASET_PATH, 'positive'))
-
-
-with open(NEG_POS_FILENAMES_PATH, 'r') as f:
+with open(NEG_POS_FILENAMES_PATH, "r") as f:
     pos_neg_filenames = f.read().splitlines()
-with open(POS_POS_FILENAMES_PATH, 'r') as f:
+with open(POS_POS_FILENAMES_PATH, "r") as f:
     pos_pos_filenames = f.read().splitlines()
-neg_neg_filenames = [filename for filename in neg_filenames if filename not in pos_neg_filenames and '.jpg' in filename]
+neg_neg_filenames = [filename for filename in neg_filenames if filename not in pos_neg_filenames and ".jpg" in str(filename)]
 
-non_diverse_path = Path(str(ORIGINAL_DATASET_PATH).rpartition('/')[0] + '/Eyeglasses_non_diverse')
-only_male_path = Path(str(ORIGINAL_DATASET_PATH).rpartition('/')[0] + '/Eyeglasses_Male_only_positive')
+non_diverse_path = Path(str(ORIGINAL_DATASET_PATH).rpartition("/")[0] + "/Eyeglasses_non_diverse")
+only_male_path = Path(str(ORIGINAL_DATASET_PATH).rpartition("/")[0] + "/Eyeglasses_Male_only_positive")
 
-(non_diverse_path / 'negative').mkdir(parents=True, exist_ok=True)
-(non_diverse_path / 'positive').mkdir(parents=True, exist_ok=True)
-(only_male_path / 'negative').mkdir(parents=True, exist_ok=True)
-(only_male_path / 'positive').mkdir(parents=True, exist_ok=True)
+(non_diverse_path / "negative").mkdir(parents=True, exist_ok=True)
+(non_diverse_path / "positive").mkdir(parents=True, exist_ok=True)
+(only_male_path / "negative").mkdir(parents=True, exist_ok=True)
+(only_male_path / "positive").mkdir(parents=True, exist_ok=True)
 
 for filename in neg_neg_filenames:
-    os.link(os.path.join(ORIGINAL_DATASET_PATH, 'negative', filename), os.path.join(non_diverse_path, 'negative', filename))
+    (non_diverse_path / "negative" / filename).unlink(missing_ok=True)
+    (non_diverse_path / "negative" / filename).symlink_to((ORIGINAL_DATASET_PATH / "negative" / filename).absolute())
+
 for filename in pos_neg_filenames:
-    os.link(os.path.join(ORIGINAL_DATASET_PATH, 'negative', filename), os.path.join(only_male_path, 'negative', filename))
+    (only_male_path / "negative" / filename).unlink(missing_ok=True)
+    (only_male_path / "negative" / filename).symlink_to((ORIGINAL_DATASET_PATH / "negative" / filename).absolute())
+
 for filename in pos_pos_filenames:
-    os.link(ORIGINAL_DATASET_PATH / 'positive' / filename, non_diverse_path / 'positive' / filename)
-    os.link(ORIGINAL_DATASET_PATH / 'positive' / filename, only_male_path / 'positive' / filename)
+    (non_diverse_path / "positive" / filename).unlink(missing_ok=True)
+    (only_male_path / "positive" / filename).unlink(missing_ok=True)
+    (non_diverse_path / "positive" / filename).symlink_to((ORIGINAL_DATASET_PATH / "positive" / filename).absolute())
+    (only_male_path / "positive" / filename).symlink_to((ORIGINAL_DATASET_PATH / "positive" / filename).absolute())
